ca8c8bdc393d7ee7084b5874514022e8fbb563bc
rtroncosogar <rodolfo.troncoso@alumnos.usm.cl>
2017-11-02 00:47:34 +0000

    Se actualiza el warning

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from flask import jsonify
 from sqlalchemy.sql import func
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/datos4.db'
 db = SQLAlchemy(app)
 
